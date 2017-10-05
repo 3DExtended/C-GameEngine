24d43871f212666e645fa8dcbb85fab31fe7ab78
3DExtended <peteresser95@me.com>
2017-10-05 23:22:15 +0200

    I added the setting which enables closing of a cylinder (by adding one vertex for each end of the cylinder and connecting it to each surrounding vertex).

--- CGameEngine/src/Engine/Util/Generators/CylinderGenerator.cpp
+++ CGameEngine/src/Engine/Util/Generators/CylinderGenerator.cpp
@@ -9,22 +9,32 @@ Mesh * CylinderGenerator::createCylinder(float radius, unsigned int numberOfVert
 	return createCylinder(radius, radius, numberOfVerts, numberOfHeightSections, heightOfSection, color);
 }
 
-Mesh * CylinderGenerator::createCylinder(float radiusBase, float radiusTop, unsigned int numberOfVerts,unsigned int numberOfHeightSections, float heightOfSection, glm::vec4 color)
+Mesh * CylinderGenerator::createCylinder(float radiusBase, float radiusTop, unsigned int numberOfVerts,unsigned int numberOfHeightSections, float heightOfSection, glm::vec4 color, bool closeTop)
 {
 	Mesh* cylinder = new Mesh();
 
 	float angle = 360.0f / numberOfVerts;
 
 
+	int id;
 	for(unsigned int y = 0 ; y <= numberOfHeightSections; y++){
 		float radius = radiusBase + (radiusTop - radiusBase) / (float)numberOfHeightSections * (float) y;
-		int id;
+
+		//create closure if needed
+		if (closeTop && y == 0) {
+			cylinder->addPoint(glm::vec3(0), glm::vec3(0,-1,0), glm::vec2(0), color, true);
+		}
+
 		for (unsigned int i = 0; i < numberOfVerts; i++) {
 			
 			glm::vec4 normal(radius, (float)y * heightOfSection, 0, 0);
 			normal = glm::rotate(0.0174533f *angle * (float)i, glm::vec3(0, 1, 0)) * normal;
 
 			id = cylinder->addPoint(glm::vec3(normal.x, normal.y, normal.z), glm::vec3(0), glm::vec2(0), color, true);
+
+			if (closeTop && y == 0 && i != 0) {
+				cylinder->addTriangle(0 , id, id-1);
+			}
 
 			if (y != 0 && i != 0) {
 				int idprev = id - 1;
@@ -39,6 +49,11 @@ Mesh * CylinderGenerator::createCylinder(float radiusBase, float radiusTop, unsi
 
 		}
 
+		//create closure if needed
+		if (closeTop && y == 0) {
+			cylinder->addTriangle(0, 1, id);
+		}
+
 		if (y != 0) {
 			//create last face by connecting last and first vertex in this row
 			//id contains newest id
@@ -51,5 +66,13 @@ Mesh * CylinderGenerator::createCylinder(float radiusBase, float radiusTop, unsi
 		}
 	}
 
+	if (closeTop) {
+		int lastVert = cylinder->addPoint(glm::vec3(0, heightOfSection * numberOfHeightSections,0), glm::vec3(0, 1, 0), glm::vec2(0), color, true);
+		for (int i = id - numberOfVerts + 2; i < id+1; i++) {
+			cylinder->addTriangle(lastVert, i, i - 1);
+		}
+		cylinder->addTriangle(lastVert, id - numberOfVerts + 1, id);
+
+	}
 	return cylinder;
 }
--- CGameEngine/src/Engine/Util/Generators/CylinderGenerator.h
+++ CGameEngine/src/Engine/Util/Generators/CylinderGenerator.h
@@ -7,7 +7,7 @@ namespace ENGINE {
 
 		public:
 			static Mesh * createCylinder(float radius, unsigned int numberOfVerts, unsigned int numberOfHeightSections, float heightOfSection, glm::vec4 color);
-			static Mesh * createCylinder(float radiusBase, float radiusTop, unsigned int numberOfVerts, unsigned int numberOfHeightSections, float heightOfSection, glm::vec4 color);
+			static Mesh * createCylinder(float radiusBase, float radiusTop, unsigned int numberOfVerts, unsigned int numberOfHeightSections, float heightOfSection, glm::vec4 color, bool closeTop = true);
 		private:
 
 
