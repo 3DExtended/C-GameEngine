eaa40f476c36c08fe18680397f7a4b573d5516b4
3DExtended <peteresser95@me.com>
2017-10-13 16:44:18 +0200

    There is no tchar in linux and since we cant test sdl2 things we can use the normal main function.

--- CGameEngine/Tests/Main.cpp
+++ CGameEngine/Tests/Main.cpp
@@ -1,12 +1,10 @@
-#include <tchar.h>	//Other entry point for windows
 #include "Engine/Util/Math.h"
 #include <iostream>
 //include all scenes
 using namespace ENGINE;
 using namespace ENGINE::UTIL;
 
-int main(int argc, _TCHAR* argv[]) {
-	
+int main (int argc, char *argv[]){
 	std::cout << "Test" << std::endl;
 	return 0;
 }
