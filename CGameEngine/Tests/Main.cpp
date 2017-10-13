b8211b5575cc7e18c258dcddc9386d319794ef92
3DExtended <peteresser95@me.com>
2017-10-13 17:14:49 +0200

    Minor progress been made

--- CGameEngine/Tests/Main.cpp
+++ CGameEngine/Tests/Main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-//include all scenes
-using namespace ENGINE;
-using namespace ENGINE::UTIL;
 
+//include all scenes
 int main (int argc, char *argv[]){
 	std::cout << "Test" << std::endl;
 	return 0;
