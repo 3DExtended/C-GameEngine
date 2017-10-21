86b152e1bcebbf153db0324faf46b8974c64b39a
3DExtended <peter.esser2@rwth-aachen.de>
2017-10-21 17:22:49 +0200

    Worked on more commenting

--- CGameEngine/src/Engine/Components/Renderer.h
+++ CGameEngine/src/Engine/Components/Renderer.h
@@ -6,14 +6,31 @@
 #include <glm/gtc/type_ptr.hpp>
 
 namespace ENGINE {
+	/// <summary>
+	/// This class is used to Render a specified model with a given shader. 
+	/// </summary>
+	/// <remarks>
+	/// Note that neither the Model nor the Shader can be used without this class and get rendered.
+	/// </remarks>
 	class Renderer : public Component{
 	public:
+		/// <summary>
+		/// Creates a new Renderer for a passed shader and a given model. 
+		/// </summary>
+		/// <param name="shaderName">The path to the shader</param>
+		/// <param name="model">A pointer to the model you wanna render</param>
 		Renderer(const std::string shaderName, Model* model);
 
+		/// <summary>
+		/// This function renders the model using the specified shader to the current context. 
+		/// </summary>
 		void Render();
 
 	private:
+		//A pointer to the shader you wanna use
 		Shader* shader;
+
+		//The model you want to render
 		Model* model;
 	};
 }
